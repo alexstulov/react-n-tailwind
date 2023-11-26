bebeb72ba4599121be358bbefe00773e3f1c356b
Alex Stulov <a.stulov.web@gmail.com>
2023-11-26 13:23:36 +0300

    Make bulbs light asynchronously

--- src/modules/app-ideas/christmasLights/index.tsx
+++ src/modules/app-ideas/christmasLights/index.tsx
@@ -2,16 +2,14 @@ import React from "react";
 import "./styles.css";
 
 const ChristmasLights = () => {
-  const bulbs = ["red", "pink", "blue", "turquoise", "mint", "green", "orange"]
-    .map(color => {
-
-      return (<div key={color} 
-        className={`garland--bulb garland--bulb__color-${color}`}
-        style={{
-          animation: `filter-animation-${color} 3s`,
-          animationIterationCount: "infinite"
-        }}></div>);
-    })
+  const colors = ["red", "pink", "blue", "turquoise", "mint", "green", "orange"];
+  const bulbs = colors.map((color, index) => (<div key={color} 
+    className={`garland--bulb garland--bulb__color-${color}`}
+    style={{
+      animation: `filter-animation-${color} 2s`,
+      animationIterationCount: "infinite",
+      animationDelay: `${index*0.3}s`
+    }}></div>))
 
   return <article className="christmas-lights">
     <h1>Christmas Lights</h1>
