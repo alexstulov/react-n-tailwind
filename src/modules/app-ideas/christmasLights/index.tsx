0e65a2f83d70c482a4fd6d53c88f6b57e51937d9
Alex Stulov <a.stulov.web@gmail.com>
2023-11-26 13:43:42 +0300

    Add lights management form with on-off toggle

--- src/modules/app-ideas/christmasLights/index.tsx
+++ src/modules/app-ideas/christmasLights/index.tsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 const ChristmasLights = () => {
+  const [blinking, setBlinking] = useState(true)
+
   const colors = ["red", "pink", "blue", "turquoise", "mint", "green", "orange"];
   const bulbs = colors.map((color, index) => (<div key={color} 
     className={`garland--bulb garland--bulb__color-${color}`}
-    style={{
+    style={blinking ? {
       animation: `filter-animation-${color} 2s`,
       animationIterationCount: "infinite",
       animationDelay: `${index*0.3}s`
-    }}></div>))
+    } : {}}></div>))
 
-  return <article className="christmas-lights">
+  return <article className="prose christmas-lights">
     <h1>Christmas Lights</h1>
     <section className="garland">
       {bulbs}
+    </section>
+    <section className="prose management">
+      <h2>Manage lights behavior</h2>
+      <div className="form-control w-52">
+        <label className="label cursor-pointer">
+          <span className="label-text">On/Off blinking</span> 
+          <input type="checkbox" className="toggle" checked={blinking} onChange={() => setBlinking(blinking => !blinking)} />
+        </label>
+      </div>
     </section>
   </article>
 };
